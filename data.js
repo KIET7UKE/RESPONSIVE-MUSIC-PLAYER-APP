e80add51dea670abd2cabf98d1fa30e122816ff5
SAHIL SHARMA <76551267+KIET7UKE@users.noreply.github.com>
2022-01-11 18:44:43 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,50 @@
+let songs = [
+{
+    name: 'Let me down slowly',
+    path: 'Assets/Musics/song 1.mp3',
+    artist: 'Alec benjamin',
+    cover: 'Assets/images/Let me down slowly.jpg'
+},
+{
+    name: 'Stunnin',
+    path: 'Assets/Musics/song 2.mp3',
+    artist: 'Curis water',
+    cover: 'Assets/images/stunnin.jpg'
+},
+{
+    name: 'Howling',
+    path: 'Assets/Musics/song 3.mp3',
+    artist: 'Cartoon',
+    cover: 'Assets/images/howling.jpg'
+},
+{
+    name: 'Lovely',
+    path: 'Assets/Musics/song 4.mp3',
+    artist: 'Billie eilish, Ft. Khalid',
+    cover: 'Assets/images/lovely.jpg'
+},
+{
+    name: 'Cheap thrills',
+    path: 'Assets/Musics/song 5.mp3',
+    artist: 'Sia, Ft. Sean paul',
+    cover: 'Assets/images/cheap thrills.jpg'
+},
+{
+    name: 'Still learning',
+    path: 'Assets/Musics/song 6.mp3',
+    artist: 'Halsey',
+    cover: 'Assets/images/still learning.jpg'
+},
+{
+    name: 'Blueberry Faygo',
+    path: 'Assets/Musics/song 7.mp3',
+    artist: 'Lil mosey',
+    cover: 'Assets/images/Blueberry Faygo.jpg'
+},
+{
+    name: 'Hate me',
+    path: 'Assets/Musics/song 8.mp3',
+    artist: 'MASN',
+    cover: 'Assets/images/Hate me.jpg'
+},
+]
